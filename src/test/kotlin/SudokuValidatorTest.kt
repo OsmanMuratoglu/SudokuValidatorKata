70b9de559276fd54a3e9cce538437e9a6d2fbcbb
Muratoglu, Osman (SE-A/34) <osman.muratoglu@volkswagen.de>
2024-04-25 10:59:27 +0200

    invalid sudoku 1 passing

--- src/test/kotlin/SudokuValidatorTest.kt
+++ src/test/kotlin/SudokuValidatorTest.kt
@@ -231,6 +231,21 @@ class MissingLetterTests {
         )
         assertEquals(false, validate(board))
     }
+    @Test
+    fun `invalid sudoku 1`() {
+        val board = arrayOf(
+            arrayOf(0, 3, 4, 6, 7, 8, 9, 1, 2),
+            arrayOf(6, 7, 2, 1, 9, 5, 3, 4, 8),
+            arrayOf(1, 9, 8, 3, 4, 2, 5, 6, 7),
+            arrayOf(8, 5, 9, 7, 6, 1, 4, 2, 3),
+            arrayOf(4, 2, 6, 8, 5, 3, 7, 9, 1),
+            arrayOf(7, 1, 3, 9, 2, 4, 8, 5, 6),
+            arrayOf(9, 6, 1, 5, 3, 7, 2, 8, 4),
+            arrayOf(2, 8, 7, 4, 1, 9, 6, 3, 5),
+            arrayOf(3, 4, 5, 2, 8, 6, 1, 7, 9)
+        )
+        assertEquals(false, validate(board))
+    }
     /*
 
 
@@ -253,21 +268,7 @@ class MissingLetterTests {
 
 
 
-        @Test
-        fun `invalid sudoku 1`() {
-            val board = arrayOf(
-                arrayOf(0, 3, 4, 6, 7, 8, 9, 1, 2),
-                arrayOf(6, 7, 2, 1, 9, 5, 3, 4, 8),
-                arrayOf(1, 9, 8, 3, 4, 2, 5, 6, 7),
-                arrayOf(8, 5, 9, 7, 6, 1, 4, 2, 3),
-                arrayOf(4, 2, 6, 8, 5, 3, 7, 9, 1),
-                arrayOf(7, 1, 3, 9, 2, 4, 8, 5, 6),
-                arrayOf(9, 6, 1, 5, 3, 7, 2, 8, 4),
-                arrayOf(2, 8, 7, 4, 1, 9, 6, 3, 5),
-                arrayOf(3, 4, 5, 2, 8, 6, 1, 7, 9)
-            )
-            assertEquals(false, validate(board))
-        }
+
 
         @Test
         fun `invalid sudoku 2`() {
