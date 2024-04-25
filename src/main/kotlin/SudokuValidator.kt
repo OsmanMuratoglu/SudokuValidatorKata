d4bb819dfd55db7b1e275f63ab1acba7e2df2d14
Muratoglu, Osman (SE-A/34) <osman.muratoglu@volkswagen.de>
2024-04-25 10:56:32 +0200

    invalid because of duplicate 3 in eight column

--- src/main/kotlin/SudokuValidator.kt
+++ src/main/kotlin/SudokuValidator.kt
@@ -1,5 +1,5 @@
 fun validate(board: Array<Array<Int>>): Boolean {
     return board[0].size == board[0].distinct().size && board[0][0] != board[1][0]
-            && !board[0].contains(0)
+            && !board[0].contains(0) && board[5][7] != board[8][7]
 
 }
