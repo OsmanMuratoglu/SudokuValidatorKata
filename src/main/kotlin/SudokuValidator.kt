7c2ca1b1c6f157a6ccdca6aaa82bbc9dbb63d024
Muratoglu, Osman (SE-A/34) <osman.muratoglu@volkswagen.de>
2024-04-25 14:19:10 +0200

    new solution passing all tests

--- src/main/kotlin/SudokuValidator.kt
+++ src/main/kotlin/SudokuValidator.kt
@@ -4,24 +4,72 @@ fun validate2(board: Array<Array<Int>>): Boolean {
 }
 
 fun validate(board: Array<Array<Int>>): Boolean {
-
     board.forEach {
         if (!it.sortedArray().contentDeepEquals(arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9))) {
             return false
         }
     }
+
     transposeMatrix(board).forEach {
         if (!it.sortedArray().contentDeepEquals(arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9))) {
             return false
         }
     }
 
-    return true
+    return checkGroups(board)
+
 }
 
 fun transposeMatrix(board: Array<Array<Int>>): Array<Array<Int>> {
-    val rows = board.size
-    val cols = board[0].size
-    return Array(cols) { j -> Array(rows) { i -> board[i][j]} }
+    return Array(board[0].size) { j -> Array(board.size) { i -> board[i][j] } }
+}
+
+fun checkGroups(board: Array<Array<Int>>): Boolean {
+    return checkGroups1(board) && checkGroups2(board) && checkGroups3(board)
+}
+
+fun checkGroups1(board: Array<Array<Int>>): Boolean {
+    var sum = 0
+
+    for (i in 1..<board.size) {
+        for (j in 0..<3) {
+            sum += board[i - 1][j]
+        }
+        if (i % 3 == 0) {
+            return sum == 45
+        }
+    }
+
+    return true
+}
+
+fun checkGroups2(board: Array<Array<Int>>): Boolean {
+    var sum = 0
+
+    for (i in 1..<board.size) {
+        for (j in 3..<6) {
+            sum += board[i - 1][j]
+        }
+        if (i % 3 == 0) {
+            return sum == 45
+        }
+    }
+
+    return true
+}
+
+fun checkGroups3(board: Array<Array<Int>>): Boolean {
+    var sum = 0
+
+    for (i in 1..<board.size) {
+        for (j in 6..<9) {
+            sum += board[i - 1][j]
+        }
+        if (i % 3 == 0) {
+            return sum == 45
+        }
+    }
+
+    return true
 }
 
